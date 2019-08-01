d40377164a9d98453787a67728f5038d992776e1
Marianne Pyon <mariannepyon@gmail.com>
2019-08-01 16:07:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  number = 0
-  while number < 0
+  number = 10
+  while number > 0
     puts "#{number} SECONDS(S)!"
-    number -= 10
+    number -= 1
 end
 puts "HAPPY NEW YEAR!"
 end
